fec10b78e2edfcd5f11dd83f757ddfa988837268
astratom <astraltom07@gmail.com>
2026-08-26 12:57:27 +0900

    이력서 연습

--- 26311012-Practice/Program.cs
+++ 26311012-Practice/Program.cs
@@ -4,7 +4,13 @@
     {
         static void Main(string[] args)
         {
+            Console.ForegroundColor = ConsoleColor.Red;
             Console.WriteLine("Hello, World!");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.SetCursorPosition(10,5);
+            Console.WriteLine("이름: 김민준");
+            Console.WriteLine("특징: 1");
+            Console.ForegroundColor = ConsoleColor.White;
         }
     }
 }
