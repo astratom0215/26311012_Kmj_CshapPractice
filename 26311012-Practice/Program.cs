43a5405d0a04230656b7f5f6c154835174bbdb2a
astratom <astraltom07@gmail.com>
2026-08-26 12:38:57 +0900

    프로그래밍 연습 프로젝트 생성

---
+++ 26311012-Practice/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _26311012_Practice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
