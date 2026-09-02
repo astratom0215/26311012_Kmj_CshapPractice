31eddf28e2072032be391ac3dae5e3b4466111d6
astratom <astraltom07@gmail.com>
2026-09-02 14:09:25 +0900

    2주차 연습

---
+++ 26311012-Kimminjun-BasicTraining/Program.cs
@@ -0,0 +1,61 @@
+﻿namespace _26311012_Kimminjun_BasicTraining
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 키워드 식별자 연습
+            //string name = "김민준";
+            //string id = "26311012";
+            //string subject = "객지프1";
+
+            //Console.WriteLine($"이력서");
+            //Console.WriteLine($"이름: {name}, 학번: {id}, 과목: {subject}");
+            Console.WriteLine("'O'");
+
+            //연산 연습
+            //Console.WriteLine(10 + 2000);
+            //Console.WriteLine(10 + "2000");
+            //Console.WriteLine("10" + 2000);
+            //Console.WriteLine("10" + "2000");
+
+            //Console.WriteLine(12345 / 1000);
+
+            // x % y = x - (x / y) * y
+            //Console.WriteLine(4 % 3);
+            //Console.WriteLine(4 % -3);
+            //Console.WriteLine(-4 % 3);
+            //Console.WriteLine(-4 % -3);
+
+            //Console.WriteLine('s');
+
+            //Console.WriteLine("");
+
+            // 변수 
+            int idNumber = 1235;
+            double score = 98.5;
+            char character = 'a';
+            string message = "안녕하세요";
+
+        }
+    }
+
+    class GameScene
+    {
+        ////생성
+        //Create();
+
+        ////초기화
+        //Init();
+
+        ////갱신
+        //Update();
+
+        ////렌더링
+        //Render();
+
+        ////파괴
+        //Destroy();
+
+    }
+}
