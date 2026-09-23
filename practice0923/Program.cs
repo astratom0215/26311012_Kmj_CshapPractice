e295672b2a75c7ee2147fa9606c74bd1355aa9ea
astratom <astraltom07@gmail.com>
2026-09-23 12:29:06 +0900

    09/23 연습

---
+++ practice0923/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Diagnostics;
+
+static void Main(string[] args)
+{
+    for (int i = 1; i<= 100;  i++)
+    {
+        if( i%2 == 0)
+        {
+            Console.WriteLine(i);
+        }
+    }    
+}
---
+++ practiceProblem0923/Program.cs
@@ -0,0 +1,96 @@
+﻿namespace practiceProblem0923
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //16번 1~100 짝수 출력
+            //for (int i = 1; i <= 100; i++)
+            //{
+            //    if (i % 2 == 0)
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+            //}
+
+
+
+            //17번 0~10출력
+            //int j = 0;
+            //while (j <= 10)
+            //{
+            //    Console.WriteLine(j);
+            //    j++;
+            //}
+
+
+
+            //18번 1~10 홀수 출력
+            //int k = 0;
+            //do
+            //{
+            //    if (k % 2 == 1)
+            //    {
+            //        Console.WriteLine(k);
+
+            //    }
+            //    k++;
+            //} while (k <= 10);
+
+
+
+            //19번 피라미드 출력
+            //int Row = 8;
+
+            //for (int i = 1; i <= Row; i++)
+            //{
+            //    for (int j = 0; j < Row - i; j++)
+            //    {
+            //        Console.Write(" ");
+            //    }
+
+            //    for (int k = 0; k < (2 * i - 1); k++)
+            //    {
+            //        Console.Write("*");
+            //    }
+
+            //    Console.WriteLine();
+            //}
+
+
+
+
+            //20번 큰수 작은수 출력
+            //int max = 0;
+            //int min = 0;
+
+            //for (int i = 0; i < 5; i++)
+            //{
+            //    Console.Write("숫자를 입력해주세요: ");
+            //    int num = int.Parse(Console.ReadLine());
+
+            //    if (i == 0)
+            //    {
+            //        max = num;
+            //        min = num;
+            //    }
+            //    else
+            //    {
+            //        if (num > max)
+            //        {
+            //            max = num;
+            //        }
+
+            //        if (num < min)
+            //        {
+            //            min = num;
+            //        }
+            //    }
+            //}
+
+            //Console.WriteLine("--------------------");
+            //Console.WriteLine("가장 큰 수: " + max);
+            //Console.WriteLine("가장 작은 수: " + min);
+        }
+    }
+}
